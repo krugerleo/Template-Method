590ec28ec257a3265187d68f02b0b90217d50bd8
Nicolas Tamalu <nicotamalu@gmail.com>
2021-11-23 18:37:24 -0300

    Adição  de comentários

--- ExtrairEstatisticas.hpp
+++ ExtrairEstatisticas.hpp
@@ -9,19 +9,26 @@
 
 class ExtrairEstatisticas {
     public:
+        /* Template method - Método Template*/
         DadosEstatisticos* obterDadosEstatisticos(const std::string& nomeArq) const;
 
     protected:
+        /* Operação concreta */
         virtual bool checarArquivo(const std::string& nomeArq) const;
 
+        /* Operação concreta */
         virtual std::ifstream abrirArquivo(const std::string& nomeArq) const;
 
+        /* Operação abstrata ou operação primitiva */
         virtual std::vector<Dado*> converterParaClasseDado(std::ifstream& arq) const = 0;
 
+        /* Operação concreta */
         DadosEstatisticos* processarDados(const std::vector<Dado*>& dados) const;
 
+        /* Operação gancho (hook operation) */
         virtual void fecharArquivo(std::ifstream& arq) const;
 
+        /* Operação concreta */
         void limparDados(std::vector<Dado*>& dados) const;
 };
 
